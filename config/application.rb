d717bb4899367e1deb77289231469408770043bc
kaya <kaya_ota@msn.com>
2012-12-26 17:48:37 -0800

    without activerecord

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module Testrails
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.frameworks -= [ :active_record ]
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
